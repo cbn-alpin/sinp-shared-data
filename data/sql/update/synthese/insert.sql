2b381b4fdb8e568c8987327e74363d30ff9959bb
Cendrine HOARAU <c.hoarau@cbn-alpin.fr>
2023-08-31 18:00:02 +0200

    feat(update synthese): add data insertion into 'cor_observer_synthese' and clean observer's uuids

--- data/sql/update/synthese/insert.sql
+++ data/sql/update/synthese/insert.sql
@@ -161,7 +161,7 @@ BEGIN
             validator,
             validation_comment,
             validation_date,
-            public.clean_uuid_observers(observers),
+            observers,
             determiner,
             id_digitiser,
             id_nomenclature_determination_method,
@@ -206,6 +206,25 @@ ALTER TABLE gn_synthese.synthese ENABLE TRIGGER tri_meta_dates_change_synthese ;
 \echo 'Enable trigger "tri_insert_calculate_sensitivity"'
 ALTER TABLE gn_synthese.synthese ENABLE TRIGGER tri_insert_calculate_sensitivity ;
 
+\echo '-------------------------------------------------------------------------------'
+\echo 'Insert data into cor_observer_synthese'
+
+INSERT INTO gn_synthese.cor_observer_synthese (id_synthese, id_role)
+    SELECT distinct
+        s.id_synthese,
+        t.id_role
+    from gn_imports.test_synthese_1 ts
+    JOIN gn_synthese.synthese s
+            ON s.unique_id_sinp = ts.unique_id_sinp
+    cross join lateral regexp_matches(ts.observers, '\[(.*?)\]', 'g') as MATCH
+    JOIN utilisateurs.t_roles t
+            ON t.uuid_role::text = match[1]
+ON CONFLICT ON CONSTRAINT pk_cor_observer_synthese DO NOTHING ;
+
+\echo '-------------------------------------------------------------------------------'
+\echo 'Clean synthese observer''s uuids'
+UPDATE gn_synthese.synthese
+SET observers = public.clean_uuid_observers(observers);
 
 \echo '----------------------------------------------------------------------------'
 \echo 'COMMIT if all is ok:'
