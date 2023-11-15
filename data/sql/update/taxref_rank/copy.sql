51364dc083a872f23331d13098b9434fff0aa6a9
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2023-11-15 17:39:38 +0100

    fix(taxref_rangs): use english 'rank' instead of french 'rangs'

--- data/sql/update/taxref_rank/copy.sql
+++ data/sql/update/taxref_rank/copy.sql
@@ -2,7 +2,7 @@
 BEGIN;
 
 \echo '-------------------------------------------------------------------------------'
-\echo 'Copy CSV data into gn_imports schema and taxref_rangs table.'
+\echo 'Copy CSV data into gn_imports schema and taxref_rank table.'
 \echo 'Rights: superuser'
 \echo 'GeoNature database compatibility : v2.4.1'
 
@@ -10,12 +10,12 @@ SET client_encoding = 'UTF8';
 
 
 \echo '-------------------------------------------------------------------------------'
-\echo 'Remove imports taxref_rangs table if already exists'
+\echo 'Remove imports taxref_rank table if already exists'
 DROP TABLE IF EXISTS gn_imports.:trImportTable ;
 
 
 \echo '-------------------------------------------------------------------------------'
-\echo 'Create imports taxref_rangs table from "taxref_rangs" with additional fields'
+\echo 'Create imports taxref_rank table from "bib_taxref_rangs" with additional fields'
 CREATE TABLE gn_imports.:trImportTable AS
     SELECT
         NULL::INT AS gid,
@@ -31,7 +31,7 @@ WITH NO DATA ;
 
 
 \echo '-------------------------------------------------------------------------------'
-\echo 'Add primary key on imports taxref_rangs table'
+\echo 'Add primary key on imports taxref_rank table'
 \set importTablePk 'pk_':trImportTable
 ALTER TABLE gn_imports.:trImportTable
 	ALTER COLUMN gid ADD GENERATED ALWAYS AS IDENTITY,
@@ -39,12 +39,12 @@ ALTER TABLE gn_imports.:trImportTable
 
 
 \echo '-------------------------------------------------------------------------------'
-\echo 'Attribute imports taxref_rangs to GeoNature DB owner'
+\echo 'Attribute imports taxref_rank to GeoNature DB owner'
 ALTER TABLE gn_imports.:trImportTable OWNER TO :gnDbOwner ;
 
 
 \echo '-------------------------------------------------------------------------------'
-\echo 'Copy CSV file to import taxref_rangs table'
+\echo 'Copy CSV file to import taxref_rank table'
 COPY gn_imports.:trImportTable (
     id_rang,
     nom_rang,
