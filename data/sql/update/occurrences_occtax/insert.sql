256a30ef71978fbb4cf36de61667620961350495
Christine BIONDA <c.bionda@cbn-alpin.fr>
2024-01-08 11:54:12 +0100

    feat(data occurrences): insert data in t_occurrences_occtax

--- data/sql/update/occurrences_occtax/insert.sql
+++ data/sql/update/occurrences_occtax/insert.sql
@@ -1,16 +1,16 @@
 BEGIN;
--- This file contain a variable "${roImportTable}" which must be replaced
+-- This file contain a variable "${ooImportTable}" which must be replaced
 -- with "sed" before passing the updated content to psql.
 
 \echo '-------------------------------------------------------------------------------'
-\echo 'Insert imported releves occtax with meta_last_action = I.'
+\echo 'Insert imported occurrences occtax with meta_last_action = I.'
 \echo 'Rights: db-owner'
 \echo 'GeoNature database compatibility : v2.4.1+'
 
 SET client_encoding = 'UTF8';
 
 \echo '-------------------------------------------------------------------------------'
-\echo 'Batch updating in "t_releves_occtax" of the imported releves occtax'
+\echo 'Batch updating in "t_occurrences_occtax" of the imported occurrences occtax'
 DO $$
 DECLARE
     step INTEGER ;
@@ -19,88 +19,83 @@ DECLARE
     affectedRows INTEGER;
 BEGIN
     -- Set dynamicly stopAt and step
-    stopAt := gn_imports.computeImportTotal('gn_imports.${roImportTable}', 'I') ;
+    stopAt := gn_imports.computeImportTotal('gn_imports.${ooImportTable}', 'I') ;
     step := gn_imports.computeImportStep(stopAt) ;
     RAISE NOTICE 'Total found: %, step used: %', stopAt, step ;
 
-    RAISE NOTICE 'Start to loop on data to insert in "t_releves_occtax" table' ;
+    RAISE NOTICE 'Start to loop on data to insert in "t_occurrences_occtax" table' ;
     WHILE offsetCnt < stopAt LOOP
 
         RAISE NOTICE '-------------------------------------------------' ;
-        RAISE NOTICE 'Try to insert % releves occtax from %', step, offsetCnt ;
+        RAISE NOTICE 'Try to insert % occurrences occtax from %', step, offsetCnt ;
 
         RAISE NOTICE '-------------------------------------------------' ;
-        RAISE NOTICE 'Inserting releves occtax data to "t_releves_occtax" if not exist' ;
-        INSERT INTO pr_occtax.t_releves_occtax (
-            unique_id_sinp_grp,
-            id_dataset,
-            id_digitiser,
-            observers_txt,
-            id_nomenclature_tech_collect_campanule,
-            id_nomenclature_grp_typ,
-            grp_method,
-            date_min,
-            date_max,
-            hour_min,
-            hour_max,
-            cd_hab,
-            altitude_min,
-            altitude_max,
-            depth_min,
-            depth_max,
-            place_name,
-            meta_device_entry,
+        RAISE NOTICE 'Inserting occurrences occtax data to "t_occurences_occtax" if not exist' ;
+        INSERT INTO pr_occtax.t_occurrences_occtax (
+            unique_id_occurence_occtax,
+            id_nomenclature_obs_technique,
+            id_nomenclature_bio_condition,
+            id_nomenclature_bio_status,
+            id_nomenclature_naturalness,
+            id_nomenclature_exist_proof,
+            id_nomenclature_diffusion_level,
+            id_nomenclature_observation_status,
+            id_nomenclature_blurring,
+            id_nomenclature_source_status,
+            id_nomenclature_behaviour,
+            determiner,
+            id_nomenclature_determination_method,
+            cd_nom,
+            nom_cite,
+            meta_v_taref,
+            sample_number_proof,
+            digital_proof,
+            non_digital_proof,
             comment,
-            geom_local,
-            geom_4326,
-            id_nomenclature_geo_object_nature,
-            precision,
-            additional_fields,
-            meta_create_date,
-            meta_update_date
-        )
-        SELECT
-            unique_id_sinp_grp,
-            code_dataset,
-            code_digitiser,
-            observers,
-            code_nomenclature_obs_technique,
-            code_nomenclature_grp_typ,
-            grp_method,
-            date_min,
-            date_max,
-            hour_min,
-            hour_max,
-            cd_hab,
-            altitude_min,
-            altitude_max,
-            depth_min,
-            depth_max,
-            place_name,
-            meta_device_entry,
-            comment_context,
-            ST_Transform(geom, 4326),
-            geom,
-            code_nomenclature_geo_object_nature,
-            precision,
             additional_fields,
             meta_create_date,
             meta_update_date,
             meta_last_action
-        FROM gn_imports.${roImportTable} AS roit
+        )
+        SELECT
+            unique_id_sinp_occtax,
+            code_nomenclature_obs_technique,
+            code_nomenclature_bio_condition,
+            code_nomenclature_bio_status,
+            code_nomenclature_naturalness,
+            code_nomenclature_exist_proof,
+            code_nomenclature_diffusion_level,
+            code_nomenclature_observation_status,
+            code_nomenclature_blurring,
+            code_nomenclature_source_status,
+            code_nomenclature_behaviour,
+            determiner,
+            code_nomenclature_determination_method,
+            cd_nom,
+            nom_cite,
+            meta_v_taxref,
+            sample_number_proof,
+            digital_proof,
+            non_digital_proof,
+            comment_description,
+            additional_fields,
+            meta_create_date,
+            meta_update_date,
+            meta_last_action
+        FROM gn_imports.${ooImportTable} AS ooit
         WHERE NOT EXISTS (
                 SELECT 'X'
-                FROM pr_occtax.t_releves_occtax AS tro
-                WHERE tro.unique_id_sinp_grp = roit.unique_id_sinp_grp
+                FROM pr_occtax.t_occurrences_occtax AS too
+                WHERE too.unique_id_occurence_occtax = ooit.unique_id_sinp_occtax
             )
-            AND roit.meta_last_action = 'I'
-        ORDER BY roit.gid ASC
+            AND ooit.meta_last_action = 'I'
+        ORDER BY ooit.gid ASC
         -- With NOT EXISTS don't use OFFSET because it's eliminate previously inserted rows.
         -- OFFSET offsetCnt
         LIMIT step ;
 
         GET DIAGNOSTICS affectedRows = ROW_COUNT;
-        RAISE NOTICE 'Inserted PARENT acquistion frameworks rows: %', affectedRows ;
+        RAISE NOTICE 'Inserted PARENT occurrences occtax rows: %', affectedRows ;
 
         offsetCnt := offsetCnt + (step) ;
     END LOOP ;
