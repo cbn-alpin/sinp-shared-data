cabe7dcecb17c597f40a2f0f59e260e46d7f5404
Christine BIONDA <c.bionda@cbn-alpin.fr>
2024-01-09 18:10:53 +0100

    fix(data): create just one gn import

---
+++ data/sql/update/occtax/copy.sql
@@ -0,0 +1,172 @@
+BEGIN;
+
+\echo '-------------------------------------------------------------------------------'
+\echo 'Copy CSV data into gn_imports schema and occtax table.'
+\echo 'Rights: superuser'
+\echo 'GeoNature database compatibility : v2.4.1'
+
+SET client_encoding = 'UTF8';
+
+
+\echo '-------------------------------------------------------------------------------'
+\echo 'Remove imports occtax table if already exists'
+DROP TABLE IF EXISTS gn_imports.:ocImportTable ;
+
+
+\echo '-------------------------------------------------------------------------------'
+\echo 'Create imports occtax table from "t_releves_occtax, t_occurrences_occtax, cor_counting_occtax" with additional fields'
+CREATE TABLE gn_imports.:ocImportTable AS
+    SELECT
+        NULL::INT AS gid,
+        unique_id_sinp_grp,
+        id_dataset,
+        id_digitiser,
+        observers_txt AS observers,
+        id_nomenclature_tech_collect_campanule,
+        id_nomenclature_grp_typ,
+        grp_method,
+        date_min,
+        date_max,
+        hour_min,
+        hour_max,
+        cd_hab,
+        altitude_min,
+        altitude_max,
+        depth_min,
+        depth_max,
+        place_name,
+        meta_device_entry,
+        tro."comment" AS comment_context,
+        geom_local AS geom,
+        id_nomenclature_geo_object_nature,
+        "precision",
+        unique_id_occurence_occtax,
+        id_nomenclature_obs_technique,
+        id_nomenclature_bio_condition,
+        id_nomenclature_bio_status,
+        id_nomenclature_naturalness,
+        id_nomenclature_exist_proof,
+        id_nomenclature_diffusion_level,
+        id_nomenclature_observation_status,
+        id_nomenclature_blurring,
+        id_nomenclature_source_status,
+        id_nomenclature_behaviour,
+        determiner,
+        id_nomenclature_determination_method,
+        cd_nom,
+        nom_cite,
+        meta_v_taxref,
+        sample_number_proof,
+        digital_proof,
+        non_digital_proof,
+        too."comment" AS comment_description,
+        unique_id_sinp_occtax,
+        id_nomenclature_life_stage,
+        id_nomenclature_sex,
+        id_nomenclature_obj_count,
+        id_nomenclature_type_count,
+        count_min,
+        count_max,
+        tro.additional_fields,
+        NULL::TIMESTAMP AS meta_create_date,
+        NULL::TIMESTAMP AS meta_update_date,
+        NULL::BPCHAR(1) AS meta_last_action
+FROM pr_occtax.t_releves_occtax tro
+	LEFT JOIN pr_occtax.t_occurrences_occtax too ON too.id_releve_occtax = tro.id_releve_occtax
+	LEFT JOIN pr_occtax.cor_counting_occtax cco ON cco.id_occurrence_occtax = too.id_occurrence_occtax
+WITH NO DATA ;
+
+
+\echo '-------------------------------------------------------------------------------'
+\echo 'Add primary key on imports occtax table'
+\set importTablePk 'pk_':ocImportTable
+ALTER TABLE gn_imports.:ocImportTable
+	ALTER COLUMN gid ADD GENERATED ALWAYS AS IDENTITY,
+	ADD CONSTRAINT :importTablePk PRIMARY KEY(gid);
+
+
+\echo '-------------------------------------------------------------------------------'
+\echo 'Create indexes on imports occtax table'
+\set uniqueIdIdx 'idx_unique_':ocImportTable'_unique_id_sinp_grp'
+CREATE UNIQUE INDEX :uniqueIdIdx
+    ON gn_imports.:ocImportTable USING btree (unique_id_sinp_grp);
+
+\set updateDateIdx 'idx_':ocImportTable'_meta_update_date'
+CREATE INDEX :updateDateIdx
+    ON gn_imports.:ocImportTable USING btree (meta_update_date);
+
+\set lastActionIdx 'idx_':ocImportTable'_meta_last_action'
+CREATE INDEX :lastActionIdx
+    ON gn_imports.:ocImportTable USING btree (meta_last_action);
+
+
+\echo '-------------------------------------------------------------------------------'
+\echo 'Attribute imports occtax to GeoNature DB owner'
+ALTER TABLE gn_imports.:ocImportTable OWNER TO :gnDbOwner ;
+
+
+\echo '-------------------------------------------------------------------------------'
+\echo 'Copy CVS file to import occtax table'
+COPY gn_imports.:ocImportTable (
+    unique_id_sinp_grp,
+    code_dataset,
+    code_digitiser,
+    observers,
+    code_nomenclature_tech_collect_campanule,
+    code_nomenclature_grp_typ,
+    grp_method,
+    date_min,
+    date_max,
+    hour_min,
+    hour_max,
+    cd_hab,
+    altitude_min,
+    altitude_max,
+    depth_min,
+    depth_max,
+    place_name,
+    meta_device_entry,
+    comment_context,
+    geom,
+    code_nomenclature_geo_object_nature,
+    precision,
+    unique_id_occurence_occtax,
+    code_nomenclature_obs_technique,
+    code_nomenclature_bio_condition,
+    code_nomenclature_bio_status,
+    code_nomenclature_naturalness,
+    code_nomenclature_exist_proof,
+    code_nomenclature_diffusion_level,
+    code_nomenclature_observation_status,
+    code_nomenclature_blurring,
+    code_nomenclature_source_status,
+    code_nomenclature_behaviour,
+    determiner,
+    code_nomenclature_determination_method,
+    cd_nom,
+    nom_cite,
+    meta_v_taxref,
+    sample_number_proof,
+    digital_proof,
+    non_digital_proof,
+    comment_description,
+    unique_id_sinp_occtax,
+    code_nomenclature_life_stage,
+    code_nomenclature_sex,
+    code_nomenclature_obj_count,
+    code_nomenclature_type_count,
+    count_min,
+    count_max,
+    additional_fields,
+    meta_create_date,
+    meta_update_date,
+    meta_last_action
+
+)
+FROM :'csvFilePath'
+WITH CSV HEADER DELIMITER E'\t' NULL '\N' ;
+
+
+\echo '----------------------------------------------------------------------------'
+\echo 'COMMIT if all is ok:'
+COMMIT;
---
+++ data/sql/update/occtax/insert.sql
@@ -0,0 +1,213 @@
+BEGIN;
+-- This file contain a variable "${ocImportTable}" which must be replaced
+-- with "sed" before passing the updated content to psql.
+
+\echo '-------------------------------------------------------------------------------'
+\echo 'Insert imported occtax with meta_last_action = I.'
+\echo 'Rights: db-owner'
+\echo 'GeoNature database compatibility : v2.4.1+'
+
+SET client_encoding = 'UTF8';
+
+\echo '-------------------------------------------------------------------------------'
+\echo 'Batch updating in "t_releves_occtax, t_occurrences_occtax, cor_counting_occtax" of the imported occtax'
+DO $$
+DECLARE
+    step INTEGER ;
+    stopAt INTEGER ;
+    offsetCnt INTEGER := 0 ;
+    affectedRows INTEGER;
+BEGIN
+    -- Set dynamicly stopAt and step
+    stopAt := gn_imports.computeImportTotal('gn_imports.${ocImportTable}', 'I') ;
+    step := gn_imports.computeImportStep(stopAt) ;
+    RAISE NOTICE 'Total found: %, step used: %', stopAt, step ;
+
+    RAISE NOTICE 'Start to loop on data to insert in "t_releves_occtax, t_occurrences_occtax, cor_counting_occtax" tables' ;
+    WHILE offsetCnt < stopAt LOOP
+
+        RAISE NOTICE '-------------------------------------------------' ;
+        RAISE NOTICE 'Try to insert % occtax from %', step, offsetCnt ;
+
+        RAISE NOTICE '-------------------------------------------------' ;
+        RAISE NOTICE 'Inserting occtax data to "t_releves_occtax, t_occurrences_occtax, cor_counting_occtax" if not exist' ;
+        INSERT INTO pr_occtax.t_releves_occtax (
+            unique_id_sinp_grp,
+            id_dataset,
+            id_digitiser,
+            observers_txt,
+            id_nomenclature_tech_collect_campanule,
+            id_nomenclature_grp_typ,
+            grp_method,
+            date_min,
+            date_max,
+            hour_min,
+            hour_max,
+            cd_hab,
+            altitude_min,
+            altitude_max,
+            depth_min,
+            depth_max,
+            place_name,
+            meta_device_entry,
+            comment,
+            geom_local,
+            geom_4326,
+            id_nomenclature_geo_object_nature,
+            precision,
+            additional_fields,
+            meta_create_date,
+            meta_update_date,
+            meta_last_action
+        )
+        SELECT
+            unique_id_sinp_grp,
+            code_dataset,
+            code_digitiser,
+            observers,
+            code_nomenclature_tech_collect_campanule,
+            code_nomenclature_grp_typ,
+            grp_method,
+            date_min,
+            date_max,
+            hour_min,
+            hour_max,
+            cd_hab,
+            altitude_min,
+            altitude_max,
+            depth_min,
+            depth_max,
+            place_name,
+            meta_device_entry,
+            comment_context,
+            ST_Transform(geom, 4326),
+            geom,
+            code_nomenclature_geo_object_nature,
+            precision,
+            additional_fields,
+            meta_create_date,
+            meta_update_date,
+            meta_last_action
+        FROM gn_imports.${ocImportTable} AS ocit
+        WHERE NOT EXISTS (
+                SELECT 'X'
+                FROM pr_occtax.t_releves_occtax AS tro
+                WHERE tro.unique_id_sinp_grp = ocit.unique_id_sinp_grp
+            )
+            AND ocit.meta_last_action = 'I'
+        ORDER BY ocit.gid ASC
+        -- With NOT EXISTS don't use OFFSET because it's eliminate previously inserted rows.
+        -- OFFSET offsetCnt
+        LIMIT step ;
+
+        INSERT INTO pr_occtax.t_occurrences_occtax (
+            unique_id_occurence_occtax,
+            id_nomenclature_obs_technique,
+            id_nomenclature_bio_condition,
+            id_nomenclature_bio_status,
+            id_nomenclature_naturalness,
+            id_nomenclature_exist_proof,
+            id_nomenclature_diffusion_level,
+            id_nomenclature_observation_status,
+            id_nomenclature_blurring,
+            id_nomenclature_source_status,
+            id_nomenclature_behaviour,
+            determiner,
+            id_nomenclature_determination_method,
+            cd_nom,
+            nom_cite,
+            meta_v_taref,
+            sample_number_proof,
+            digital_proof,
+            non_digital_proof,
+            comment,
+            additional_fields,
+            meta_create_date,
+            meta_update_date,
+            meta_last_action
+        )
+        SELECT
+            unique_id_occurence_occtax,
+            code_nomenclature_obs_technique,
+            code_nomenclature_bio_condition,
+            code_nomenclature_bio_status,
+            code_nomenclature_naturalness,
+            code_nomenclature_exist_proof,
+            code_nomenclature_diffusion_level,
+            code_nomenclature_observation_status,
+            code_nomenclature_blurring,
+            code_nomenclature_source_status,
+            code_nomenclature_behaviour,
+            determiner,
+            code_nomenclature_determination_method,
+            cd_nom,
+            nom_cite,
+            meta_v_taxref,
+            sample_number_proof,
+            digital_proof,
+            non_digital_proof,
+            comment_description,
+            additional_fields,
+            meta_create_date,
+            meta_update_date,
+            meta_last_action
+        FROM gn_imports.${ocImportTable} AS ocit
+        WHERE NOT EXISTS (
+                SELECT 'X'
+                FROM pr_occtax.t_occurrences_occtax AS too
+                WHERE too.unique_id_occurence_occtax = ocit.unique_id_occurence_occtax
+            )
+            AND ocit.meta_last_action = 'I'
+        ORDER BY ocit.gid ASC
+        -- With NOT EXISTS don't use OFFSET because it's eliminate previously inserted rows.
+        -- OFFSET offsetCnt
+        LIMIT step ;
+
+         INSERT INTO pr_occtax.cor_counting_occtax (
+            unique_id_sinp_occtax,
+            code_nomenclature_life_stage,
+            code_nomenclature_sex,
+            code_nomenclature_obj_count,
+            code_nomenclature_type_count,
+            count_min,
+            count_max,
+            additional_fields,
+            meta_create_date,
+            meta_update_date,
+            meta_last_action
+        )
+        SELECT
+            unique_id_sinp_occtax,
+            code_nomenclature_life_stage,
+            code_nomenclature_sex,
+            code_nomenclature_obj_count,
+            code_nomenclature_type_count,
+            count_min,
+            count_max,
+            additional_fields,
+            meta_create_date,
+            meta_update_date,
+            meta_last_action
+        FROM gn_imports.${ocImportTable} AS ocit
+        WHERE NOT EXISTS (
+                SELECT 'X'
+                FROM pr_occtax.cor_counting_occtax AS cco
+                WHERE cco.unique_id_sinp_occtax = ocit.unique_id_sinp_occtax
+            )
+            AND ocit.meta_last_action = 'I'
+        ORDER BY ocit.gid ASC
+        -- With NOT EXISTS don't use OFFSET because it's eliminate previously inserted rows.
+        -- OFFSET offsetCnt
+        LIMIT step ;
+
+        GET DIAGNOSTICS affectedRows = ROW_COUNT;
+        RAISE NOTICE 'Inserted PARENT occtax rows: %', affectedRows ;
+
+        offsetCnt := offsetCnt + (step) ;
+    END LOOP ;
+END
+$$ ;
+
+\echo '----------------------------------------------------------------------------'
+\echo 'COMMIT if all is ok:'
+COMMIT;
